014d051ff1e957f2718509802cfd00481d22d4b2
Nino <ninoacev@gmail.com>
2025-03-01 00:58:48 +0100

    bugfix/poke-10-netlify-fix

--- next.config.ts
+++ next.config.ts
@@ -16,6 +16,19 @@ const nextConfig: NextConfig = {
       },
     ],
   },
+  webpack: (config) => {
+    config.module.rules.push({
+      test: /\.(svg|png|jpg|gif|webp)$/,
+      type: "asset/resource",
+    });
+    return config;
+  },
+};
+
+module.exports = {
+  images: {
+    domains: ["https://firstgenn.netlify.app/"],
+  },
 };
 
 export default nextConfig;
