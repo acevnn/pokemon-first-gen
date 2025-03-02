8b9a80e524adf43b103893635199dd4f7717657e
Nino <ninoacev@gmail.com>
2025-03-02 11:12:32 +0100

    changes for netlify

--- .next/server/server-reference-manifest.js
+++ .next/server/server-reference-manifest.js
@@ -1,1 +1,1 @@
-self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"v0WQqRVjY2LU/mSyv8z1A845+3bkrErgOVAik8tvQo0=\"\n}"
+self.__RSC_SERVER_MANIFEST="{\"node\":{},\"edge\":{},\"encryptionKey\":\"process.env.NEXT_SERVER_ACTIONS_ENCRYPTION_KEY\"}"
