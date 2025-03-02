c6c866400bf639ebb06880980cfaa2f2c8b62e21
Nino <ninoacev@gmail.com>
2025-03-02 12:22:19 +0100

    merge conflicts

--- src/app/PokemonList/page.tsx
+++ src/app/PokemonList/page.tsx
@@ -1,9 +1,20 @@
 import PokemonList from "@/app/PokemonList/PokemonList";
+import { fetchPokemonData } from "@/utils/pokemonData";
 import "./PokemonList.module.scss";
 
 export default async function showAllPokePage() {
   try {
-    return <PokemonList limit={20} totalItems={151} />;
+    const totalItems = 151;
+    const limit = 20;
+    const initialPokemon = await fetchPokemonData({ offset: 0, limit });
+
+    return (
+      <PokemonList
+        totalItems={totalItems}
+        limit={limit}
+        initialPokemon={initialPokemon}
+      />
+    );
   } catch (error) {
     console.error("Failed to fetch Pokémon data:", error);
     return <p>Error loading Pokémon data</p>;
